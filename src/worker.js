4074706543794144cafe51e39dff0833a1af6354
abskmj <abskmj@gmail.com>
2022-10-19 07:13:38 +0000

    Add api to parse using video id

--- src/worker.js
+++ src/worker.js
@@ -8,7 +8,20 @@ addEventListener('fetch', (event) => {
   )
 })
 
-async function handleRequest (request) {
+async function getLiveStream(url) {
+  const response = await fetch(url)
+
+  if (response.ok) {
+    const text = await response.text()
+    const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
+
+    return stream
+  } else {
+    throw Error(`Youtube URL (${url}) failed with status: ${response.status}`)
+  }
+}
+
+async function handleRequest(request) {
   const { pathname } = new URL(request.url)
 
   if (pathname.startsWith('/channel/')) {
@@ -17,24 +30,21 @@ async function handleRequest (request) {
 
     if (channel !== '') {
       const url = `https://www.youtube.com/channel/${channel}/live`
-
-      const response = await fetch(url, {
-        cf: {
-          cacheTtl: 10800, // 3 hours
-          cacheEverything: true
-        }
-      })
-
-      if (response.ok) {
-        const text = await response.text()
-        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
-
-        return Response.redirect(stream, 302)
-      } else {
-        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`)
-      }
+      const stream = await getLiveStream(url)
+      return Response.redirect(stream, 302)
     } else {
       throw Error(`Channel ID not found: ${pathname}`)
+    }
+  } else if (pathname.startsWith('/video/')) {
+    // supported api /video/:id.m3u8
+    const video = pathname.split('/')?.[2]?.split('.')?.[0]
+
+    if (video !== '') {
+      const url = `https://www.youtube.com/watch?v=${video}`
+      const stream = await getLiveStream(url)
+      return Response.redirect(stream, 302)
+    } else {
+      throw Error(`Video ID not found: ${pathname}`)
     }
   } else {
     throw Error(`Path not found: ${pathname} `)
