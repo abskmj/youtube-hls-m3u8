329023f962a322a6d7c983413494dde92210f766
abskmj <abskmj@gmail.com>
2023-02-10 17:30:01 +0000

    push metrics to GA

--- src/app.js
+++ src/app.js
@@ -16,8 +16,23 @@ const getLiveStream = async (url) => {
   }
 }
 
+const track = async (event) => {
+  await fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${process.env.GA_MEASUREMENT_ID}&api_secret=${process.env.GA_API_SECRET}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      client_id: process.env.GA_CLIENT_ID,
+      events: [event]
+    })
+  })
+}
+
 app.get('/channel/:id.m3u8', async (req, res, nxt) => {
   try {
+    track({
+      name: 'feed',
+      params: { channel: req.params.id }
+    })
+
     const url = `https://www.youtube.com/channel/${req.params.id}/live`
     const stream = await getLiveStream(url)
 
@@ -29,6 +44,11 @@ app.get('/channel/:id.m3u8', async (req, res, nxt) => {
 
 app.get('/video/:id.m3u8', async (req, res, nxt) => {
   try {
+    track({
+      name: 'feed',
+      params: { video: req.params.id }
+    })
+
     const url = `https://www.youtube.com/watch?v=${req.params.id}`
     const stream = await getLiveStream(url)
 
