e006963b644dd136429f3df3dca232414d0c92c6
abskmj <abskmj@gmail.com>
2023-06-19 11:39:30 +0000

    disable analytics

--- src/app.js
+++ src/app.js
@@ -28,50 +28,51 @@ const getLiveStream = async (url) => {
     return data
   }
 }
-const { GA_MEASUREMENT_ID, GA_API_SECRET, GA_CLIENT_ID } = process.env
 
-const track = (user, event) => {
-  return fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${GA_MEASUREMENT_ID}&api_secret=${GA_API_SECRET}`, {
-    method: 'POST',
-    body: JSON.stringify({
-      client_id: GA_CLIENT_ID,
-      user_id: user.id,
-      events: [event, {
-        name: 'client',
-        params: user.properties
-      }]
-    })
-  })
-}
+// const { GA_MEASUREMENT_ID, GA_API_SECRET, GA_CLIENT_ID } = process.env
+
+// const track = (user, event) => {
+//   return fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${GA_MEASUREMENT_ID}&api_secret=${GA_API_SECRET}`, {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       client_id: GA_CLIENT_ID,
+//       user_id: user.id,
+//       events: [event, {
+//         name: 'client',
+//         params: user.properties
+//       }]
+//     })
+//   })
+// }
 
 app.use(require('express-status-monitor')())
 
-app.use((req, res, nxt) => {
-  // console.log('headers:', req.headers)
+// app.use((req, res, nxt) => {
+//   // console.log('headers:', req.headers)
 
-  req.user = {
-    id: req.headers['cf-connecting-ip'] || req.ip,
-    properties: {
-      country: req.headers['cf-ipcountry'],
-      ua: req.headers['user-agent']
-    }
-  }
+//   req.user = {
+//     id: req.headers['cf-connecting-ip'] || req.ip,
+//     properties: {
+//       country: req.headers['cf-ipcountry'],
+//       ua: req.headers['user-agent']
+//     }
+//   }
 
-  nxt()
-})
+//   nxt()
+// })
 
 app.get('/channel/:id.m3u8', async (req, res, nxt) => {
   try {
     const url = `https://www.youtube.com/channel/${req.params.id}/live`
     const { name, stream } = await getLiveStream(url)
 
-    await track(req.user, {
-      name: 'feed',
-      params: {
-        engagement_time_msec: '1',
-        name
-      }
-    })
+    // await track(req.user, {
+    //   name: 'feed',
+    //   params: {
+    //     engagement_time_msec: '1',
+    //     name
+    //   }
+    // })
 
     if (stream) {
       res.redirect(stream)
@@ -88,13 +89,13 @@ app.get('/video/:id.m3u8', async (req, res, nxt) => {
     const url = `https://www.youtube.com/watch?v=${req.params.id}`
     const { name, stream } = await getLiveStream(url)
 
-    await track(req.user, {
-      name: 'feed',
-      params: {
-        engagement_time_msec: '1',
-        name
-      }
-    })
+    // await track(req.user, {
+    //   name: 'feed',
+    //   params: {
+    //     engagement_time_msec: '1',
+    //     name
+    //   }
+    // })
 
     if (stream) {
       res.redirect(stream)
